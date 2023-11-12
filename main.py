d994c35eb93269202d03bf4f245ceb0a371fe3c3
adn87 <aaghadieh@gmail.com>
2023-11-12 09:43:22 -0500

    Flash_Card_App_004
    - created a method so that when the user clicks on the word they know it will save their progress in a csv file. That way when they user re-runs the program the words that they didn't know will show up again.

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
+current_card = {}
+to_learn = {}
 # Flash Cards
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
-current_card ={}
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 def next_card():
@@ -26,6 +31,14 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
 
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
 # UI setup
 window = Tk()
 window.title("Flash Card Application")
@@ -35,16 +48,15 @@ flip_timer = window.after(3000, func=flip_card)
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
-card_background = canvas.create_image(400,263, image=card_front_img)
-canvas.config(bg= BACKGROUND_COLOR, highlightthickness=0)
+card_background = canvas.create_image(400, 263, image=card_front_img)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "italic"))
 canvas.grid(row=0, column=0, columnspan=2)
 
-
 # Button
 right_img = PhotoImage(file="images/right.png")
-right_button = Button(image=right_img, highlightthickness=0, bg=BACKGROUND_COLOR, command=next_card)
+right_button = Button(image=right_img, highlightthickness=0, bg=BACKGROUND_COLOR, command=is_known)
 right_button.grid(row=1, column=1)
 wrong_img = PhotoImage(file="images/wrong.png")
 wrong_button = Button(image=wrong_img, highlightthickness=0, bg=BACKGROUND_COLOR, command=next_card)
