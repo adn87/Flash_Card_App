7e1ab0d126037bbf784156bcfe633c270e97e8a7
adn87 <aaghadieh@gmail.com>
2023-11-12 09:29:24 -0500

    Flash_Card_App_003
    - added the flip card method so that the card flips after waiting 3 seconds

--- main.py
+++ main.py
@@ -7,21 +7,35 @@ BACKGROUND_COLOR = "#B1DDC6"
 # Flash Cards
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
+current_card ={}
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    global current_card
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 # UI setup
 window = Tk()
 window.title("Flash Card Application")
 window.config(pady=50, padx=50, bg=BACKGROUND_COLOR, highlightthickness=0)
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400,263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400,263, image=card_front_img)
 canvas.config(bg= BACKGROUND_COLOR, highlightthickness=0)
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "italic"))
